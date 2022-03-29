fae941d4ddec0b873d9448729a8ad28552673678
varadib02 <barkabas02@gmail.com>
2022-03-29 10:39:40 +0200

    Add project files.

---
+++ Rostok/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Rostok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<adatok> beo = new List<adatok>();
+            foreach (var sor in File.ReadAllLines("rostok.txt").Skip(1))
+            {
+                beo.Add(new adatok(sor));
+            }
+        }
+    }
+}
---
+++ Rostok/adatok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rostok
+{
+    class adatok
+    {
+        public string megnev { get; set; }
+        public string kat { get; set; }
+        public string egyseg { get; set; }
+        public double rost { get; set; }
+        public adatok(string sor)
+        {
+            string[] db = sor.Split(';');
+            megnev = db[0];
+            kat = db[1];
+            egyseg = db[2];
+            rost = Convert.ToDouble(db[3]);
+        }
+    }
+}
