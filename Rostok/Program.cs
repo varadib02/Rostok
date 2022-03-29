6d78e6cf8f2281838acff9e68c2ede98048b5e52
varadib02 <barkabas02@gmail.com>
2022-03-29 13:05:35 +0200

    9.f

--- Rostok/Program.cs
+++ Rostok/Program.cs
@@ -18,21 +18,36 @@ namespace Rostok
             }
             Console.WriteLine($"3. feladat: Élelmiszerek száma: {beo.Count}");
 
-            Console.WriteLine($"4. feladat: nem 100g-os egység: {beo.Where(x=>x.egyseg!="100g").Count()}");
+            Console.WriteLine($"4. feladat: nem 100g-os egység: {beo.Where(x => x.egyseg != "100g").Count()}");
 
-            Console.WriteLine($"5.feladat: Frissgyümölcsök átlagos rosttartalma: {beo.Where(x => x.egyseg == "100g" && x.kat=="Friss gyümölcsök").Average(x=>x.rost)}g");
+            Console.WriteLine($"5.feladat: Frissgyümölcsök átlagos rosttartalma: {beo.Where(x => x.egyseg == "100g" && x.kat == "Friss gyümölcsök").Average(x => x.rost)}g");
 
             Console.WriteLine($"6.feladat: Kérek egy karakter láncot:");
-            string bekert_adat=Console.ReadLine();
+            string bekert_adat = Console.ReadLine();
 
-            Console.WriteLine($"7.feladat: Kategóriák  száma: {beo.GroupBy(x=>x.kat).Count()}");
+            Console.WriteLine($"7.feladat: Kategóriák  száma: {beo.GroupBy(x => x.kat).Count()}");
 
             Console.WriteLine($"8.feladat: Statisztika");
-            Console.WriteLine($"\tAszalt gyümölcsök - {beo.Where(x=>x.kat== "Aszalt gyümölcsök").Count()}");
-            Console.WriteLine($"\tFriss gyümölcsök - {beo.Where(x=>x.kat== "Friss gyümölcsök").Count()}");
-            Console.WriteLine($"\tGabonák és lisztek - {beo.Where(x=>x.kat== "Gabonák és lisztek").Count()}");
-            Console.WriteLine($"\tZöldségek - {beo.Where(x=>x.kat== "Zöldségek").Count()}");
-            Console.WriteLine($"\tMagvak - {beo.Where(x=>x.kat== "Magvak").Count()}");
+            Console.WriteLine($"\tAszalt gyümölcsök - {beo.Where(x => x.kat == "Aszalt gyümölcsök").Count()}");
+            Console.WriteLine($"\tFriss gyümölcsök - {beo.Where(x => x.kat == "Friss gyümölcsök").Count()}");
+            Console.WriteLine($"\tGabonák és lisztek - {beo.Where(x => x.kat == "Gabonák és lisztek").Count()}");
+            Console.WriteLine($"\tZöldségek - {beo.Where(x => x.kat == "Zöldségek").Count()}");
+            Console.WriteLine($"\tMagvak - {beo.Where(x => x.kat == "Magvak").Count()}");
+
+            Console.WriteLine($"9.feladat: Rostok100g.txt");
+            StreamWriter sw = File.CreateText("Rostok100g.txt");
+            sw.WriteLine("Megnevezés;Kategória;Rost");
+            
+            foreach (var i in beo)
+            {
+                if (i.egyseg== "100g")
+                {
+                    sw.WriteLine($"{i.megnev};{i.kat};{i.rost}");
+                }
+            }
+
+
+
         }
     }
 }
