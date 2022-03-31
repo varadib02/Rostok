257f997a19e4d1859e6890e8b2be030f013e3dca
varadib02 <barkabas02@gmail.com>
2022-03-31 09:48:36 +0200

    9.f

--- Rostok/Program.cs
+++ Rostok/Program.cs
@@ -52,25 +52,20 @@ namespace Rostok
 
             Console.WriteLine($"7.feladat: Kategóriák  száma: {beo.GroupBy(x => x.kat).Count()}");
 
-                Console.WriteLine($"8.feladat: Statisztika");
-                Console.WriteLine($"\tAszalt gyümölcsök - {beo.Where(x => x.kat == "Aszalt gyümölcsök").Count()}");
-                Console.WriteLine($"\tFriss gyümölcsök - {beo.Where(x => x.kat == "Friss gyümölcsök").Count()}");
-                Console.WriteLine($"\tGabonák és lisztek - {beo.Where(x => x.kat == "Gabonák és lisztek").Count()}");
-                Console.WriteLine($"\tZöldségek - {beo.Where(x => x.kat == "Zöldségek").Count()}");
-                Console.WriteLine($"\tMagvak - {beo.Where(x => x.kat == "Magvak").Count()}");
+            Console.WriteLine($"8.feladat: Statisztika");
+            Console.WriteLine($"\tAszalt gyümölcsök - {beo.Where(x => x.kat == "Aszalt gyümölcsök").Count()}");
+            Console.WriteLine($"\tFriss gyümölcsök - {beo.Where(x => x.kat == "Friss gyümölcsök").Count()}");
+            Console.WriteLine($"\tGabonák és lisztek - {beo.Where(x => x.kat == "Gabonák és lisztek").Count()}");
+            Console.WriteLine($"\tZöldségek - {beo.Where(x => x.kat == "Zöldségek").Count()}");
+            Console.WriteLine($"\tMagvak - {beo.Where(x => x.kat == "Magvak").Count()}");
 
-                Console.WriteLine($"9.feladat: Rostok100g.txt");
-                StreamWriter sw = File.CreateText("Rostok100g.txt");
-                sw.WriteLine("Megnevezés;Kategória;Rost");
-
-                foreach (var a in beo)
-                {
-                    if (a.egyseg == "100g")
-                    {
-                        sw.Write($"{a.megnev};{a.kat};{a.rost}");
-                    }
-                }
-                Console.ReadKey();
+            Console.WriteLine($"9.feladat: Rostok100g.txt");
+            StreamWriter sw=File.CreateText("Rostok100g.txt");
+            sw.Close();
+            List<string> szures = new List<string>();
+            szures.Add("Megnevezés;Kategória;Rost");
+            beo.Where(x=>x.egyseg=="100g").ToList().ForEach(x=>szures.Add($"{x.megnev};{x.kat};{x.rost}"));
+            File.WriteAllLines("Rostok100g.txt",szures);
 
             
 
